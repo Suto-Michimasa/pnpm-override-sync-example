08a4c8d3a5c700de8a48bf6f55d965d204b21667
michimasa_suto <michimasa_suto@pkshatech.com>
2026-04-03 12:09:05 +0900

    refactor: move scripts from .github/scripts to scripts/

--- scripts/vitest.config.ts
+++ scripts/vitest.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
-    include: ['.github/scripts/**/*.test.ts'],
+    include: ['scripts/**/*.test.ts'],
   },
 })
